9390b0bd0e01c4bcd24a61212bb7260c97de8e38
hmkrivoj <krivoj@hm.edu>
2018-03-23 22:36:16 +0100

    Basic tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,20 +10,126 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:calculator/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    /*// Build our app and trigger a frame.
+  testWidgets('ADD test', (WidgetTester tester) async {
     await tester.pumpWidget(new MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    expect(find.text('0'), findsNWidgets(3));
+    expect(find.text('1+2'), findsNothing);
+    expect(find.text('3.0'), findsNothing);
+    await tester.tap(find.text('1'));
     await tester.pump();
+    await tester.tap(find.text('+'));
+    await tester.pump();
+    await tester.tap(find.text('2'));
+    await tester.pump();
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1+2'), findsOneWidget);
+    expect(find.text('3.0'), findsOneWidget);
+    await tester.tap(find.text('='));
+    await tester.pump();
+    expect(find.text('1+2'), findsNothing);
+    expect(find.text('3.0'), findsNWidgets(2));
+  });
+  testWidgets('SUB test', (WidgetTester tester) async {
+    await tester.pumpWidget(new MyApp());
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);*/
+    expect(find.text('0'), findsNWidgets(3));
+    expect(find.text('1-2'), findsNothing);
+    expect(find.text('-1.0'), findsNothing);
+    await tester.tap(find.text('1'));
+    await tester.pump();
+    await tester.tap(find.text('-'));
+    await tester.pump();
+    await tester.tap(find.text('2'));
+    await tester.pump();
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1-2'), findsOneWidget);
+    expect(find.text('-1.0'), findsOneWidget);
+    await tester.tap(find.text('='));
+    await tester.pump();
+    expect(find.text('1-2'), findsNothing);
+    expect(find.text('-1.0'), findsNWidgets(2));
+  });
+  testWidgets('MUL test', (WidgetTester tester) async {
+    await tester.pumpWidget(new MyApp());
+
+    expect(find.text('0'), findsNWidgets(3));
+    expect(find.text('1*2'), findsNothing);
+    expect(find.text('2.0'), findsNothing);
+    await tester.tap(find.text('1'));
+    await tester.pump();
+    await tester.tap(find.text('*'));
+    await tester.pump();
+    await tester.tap(find.text('2'));
+    await tester.pump();
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1*2'), findsOneWidget);
+    expect(find.text('2.0'), findsOneWidget);
+    await tester.tap(find.text('='));
+    await tester.pump();
+    expect(find.text('1*2'), findsNothing);
+    expect(find.text('2.0'), findsNWidgets(2));
+  });
+  testWidgets('DIV test', (WidgetTester tester) async {
+    await tester.pumpWidget(new MyApp());
+
+    expect(find.text('0'), findsNWidgets(3));
+    expect(find.text('1/2'), findsNothing);
+    expect(find.text('0.5'), findsNothing);
+    await tester.tap(find.text('1'));
+    await tester.pump();
+    await tester.tap(find.text('/'));
+    await tester.pump();
+    await tester.tap(find.text('2'));
+    await tester.pump();
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1/2'), findsOneWidget);
+    expect(find.text('0.5'), findsOneWidget);
+    await tester.tap(find.text('='));
+    await tester.pump();
+    expect(find.text('1/2'), findsNothing);
+    expect(find.text('0.5'), findsNWidgets(2));
+  });
+  testWidgets('FLOAT test', (WidgetTester tester) async {
+    await tester.pumpWidget(new MyApp());
+
+    expect(find.text('0'), findsNWidgets(3));
+    expect(find.text('0.5'), findsNothing);
+    await tester.tap(find.text('.'));
+    await tester.pump();
+    await tester.tap(find.text('5'));
+    await tester.pump();
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('0.5'), findsNWidgets(2));
+  });
+  testWidgets('CE test', (WidgetTester tester) async {
+    await tester.pumpWidget(new MyApp());
+
+    expect(find.text('0'), findsNWidgets(3));
+    expect(find.text('123'), findsNothing);
+    await tester.tap(find.text('1'));
+    await tester.pump();
+    await tester.tap(find.text('2'));
+    await tester.pump();
+    await tester.tap(find.text('3'));
+    await tester.pump();
+    expect(find.text('123'), findsOneWidget);
+    expect(find.text('123.0'), findsOneWidget);
+    await tester.tap(find.text('CE'));
+    await tester.pump();
+    expect(find.text('12'), findsOneWidget);
+    expect(find.text('12.0'), findsOneWidget);
+    await tester.tap(find.text('3'));
+    await tester.pump();
+    await tester.tap(find.text('4'));
+    await tester.pump();
+    expect(find.text('1234'), findsOneWidget);
+    expect(find.text('1234.0'), findsOneWidget);
+    await tester.longPress(find.text('CE'));
+    await tester.pump();
+    expect(find.text('0'), findsNWidgets(2));
+    expect(find.text('0.0'), findsOneWidget);
+    expect(find.text('1234'), findsNothing);
+    expect(find.text('1234.0'), findsNothing);
   });
 }
